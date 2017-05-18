894c25dec0e4468a312bf53f24ebfff949214c9a
Daniel Castro <daniel@ignite.mx>
2017-05-18 16:13:32 -0500

    Make functional_tests an app, use LiverServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,14 +1,16 @@
+from django.test import LiveServerTestCase
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):  #1
+class NewVisitorTest(LiveServerTestCase):
 
-    def setUp(self):  #2
+    def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(5)
 
-    def tearDown(self):  #3
+    def tearDown(self):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
@@ -16,13 +18,13 @@ class NewVisitorTest(unittest.TestCase):  #1
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  #4
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url) 
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)  #5
+        self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
@@ -65,6 +67,3 @@ class NewVisitorTest(unittest.TestCase):  #1
 
         # She visits that URL - her to-do list is still there
 
-
-if __name__ == '__main__':  #7
-    unittest.main(warnings='ignore')  #8
