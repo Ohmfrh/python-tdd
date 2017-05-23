c261544158f9c630a05db894ba77fec867b473ad
Daniel Castro <daniel@ignite.mx>
2017-05-22 19:40:03 -0500

    Added lists to items

--- lists/models.py
+++ lists/models.py
@@ -3,4 +3,7 @@ from django.db import models
 
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey('lists.List', default=None)
 
+class List(models.Model):
+    pass
