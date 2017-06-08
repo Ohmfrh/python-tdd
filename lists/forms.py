57f7880e88853c03f81854091ae010a7a24ce545
Daniel Castro <daniel@ignite.mx>
2017-06-08 13:35:43 -0500

    Use form in all views, back to working state

--- lists/forms.py
+++ lists/forms.py
@@ -6,16 +6,22 @@ from lists.models import Item
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
+    
+#    def __init__(self, *args, **kwargs):
+#        super(ItemForm, self).__init__(*args, **kwargs)
 
     class Meta:
         model = Item
         fields = ('text',)
         widgets = {
             'text': forms.fields.TextInput(attrs={
-                'placeholder': 'Enter a to-do item',
-                'class': 'form-control input-lg',
-            }),
+                    'placeholder': 'Enter a to-do item',
+                    'class': 'form-control input-lg',
+                }
+            )
         }
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
         }
+        # self.fields['text'].required = False
+
