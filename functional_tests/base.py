61fe1ce5b25daf7b97524e5986a3bd0249f5aed8
Daniel Castro <daniel@ignite.mx>
2017-06-15 16:05:52 -0500

    Validate emails, connects to remote db

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -10,6 +10,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
+from .server_tools import reset_database
+
 MAX_WAIT = 10
 
 
@@ -30,10 +32,10 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            setattr(self, 'live_server_url', 'http://' + self.staging_server)
+            reset_database(self.staging_server)
     def tearDown(self):
         self.browser.quit()
 
