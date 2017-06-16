c0e54dbf34bf8c9f679995756e03e3a7e9ddb4f9
Daniel Castro <daniel@ignite.mx>
2017-06-15 19:00:23 -0500

    url, placeholder view, and first-cut templates for lists

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -123,6 +123,7 @@ class ListViewTest(TestCase):
         self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
+
 class NewListTest(TestCase):
 
     def test_saving_a_POST_request(self):
@@ -160,4 +161,11 @@ class NewListTest(TestCase):
         self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
+
+
+class MyListsTest(TestCase):
+
+    def test_my_lists_url_renders_my_list_template(self):
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertTemplateUsed(response, 'my_lists.html')
 
