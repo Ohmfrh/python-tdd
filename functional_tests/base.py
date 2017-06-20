76eec7c617b7b7bc223c1c6d8fcc044be2637b43
Daniel Castro <daniel@ignite.mx>
2017-06-20 18:00:18 -0500

    Add screenshot on failure to FT runner

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -3,6 +3,7 @@ import sys
 import time
 import unittest
 from unittest import skip
+from datetime import datetime
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
@@ -14,6 +15,10 @@ from selenium.webdriver.common.keys import Keys
 from .server_tools import reset_database
 
 MAX_WAIT = 10
+
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 
 
 def wait(fn):
@@ -37,8 +42,42 @@ class FunctionalTest(StaticLiveServerTestCase):
         if self.staging_server:
             setattr(self, 'live_server_url', 'http://' + self.staging_server)
             reset_database(self.staging_server)
+
     def tearDown(self):
+        if self._test_has_failed():
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to_window(handle)
+                self.take_screenshot()
+                self.dump_html()
         self.browser.quit()
+        super().tearDown()
+
+    def take_screenshot(self):
+        filename = self._get_filename() + '.png'
+        print('screenshotting to', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+    def dump_html(self):
+        filename = self._get_filename() + '.html'
+        print('dumping page HTML to', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+        return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+            folder=SCREEN_DUMP_LOCATION,
+            classname=self.__class__.__name__,
+            method=self._testMethodName,
+            windowid=self._windowid,
+            timestamp=timestamp
+        )
+
+    def _test_has_failed(self):
+        return any(error for (method, error) in self._outcome.errors)
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
@@ -80,3 +119,4 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.wait_for_row_in_list_table(
             f'{item_number}: {item_text}'
         )
+
